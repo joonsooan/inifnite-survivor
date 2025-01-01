2798517eb6822e883b1d3048942b524b07cd1746
joonsooan <joonsooan@gmail.com>
2025-01-01 11:49:11 +0900

    Player Animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,15 @@ public class Player : MonoBehaviour
     public float moveSpeed;
 
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Animator anim;
     private Vector2 inputVec;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -24,5 +28,15 @@ public class Player : MonoBehaviour
     private void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+    }
+
+    private void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude); // 벡터의 크기만 전달
+
+        if (inputVec.x != 0)
+        {
+            sr.flipX = inputVec.x < 0;
+        }
     }
 }
