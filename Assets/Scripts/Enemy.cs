b51e4d500a1045dffe55a1504ae8bc3a3e26d6e6
joonsooan <joonsooan@gmail.com>
2025-01-01 18:57:22 +0900

    Made Enemy Movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D target;
+
+    private bool isLive = true;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isLive)
+            return;
+
+        Vector2 dirVec = target.position - rb.position;
+        Vector2 nextVec = dirVec.normalized * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + nextVec);
+        rb.velocity = Vector2.zero; // 외부 힘이 이동에 영향을 주지 않도록 속도 제거
+    }
+
+    private void LateUpdate()
+    {
+        if (!isLive)
+            return;
+
+        sr.flipX = target.position.x < rb.position.x;
+    }
+}
--- Assets/Scripts/Reposition.cs
+++ Assets/Scripts/Reposition.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+    private Collider2D coll;
+    private int mapLength;
+
+    private void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+        mapLength = 20;
+    }
+
     // 플레이어 위치에 따라 타일맵을 이동시키는 코드
     private void OnTriggerExit2D(Collider2D collision)
     {
@@ -16,25 +25,30 @@ public class Reposition : MonoBehaviour
         float diffX = Mathf.Abs(playerPosition.x - myPos.x);
         float diffY = Mathf.Abs(playerPosition.y - myPos.y);
 
-        // 오브젝트 기준 플레이어의 위치
+        // 플레이어의 이동 방향
         Vector3 playerDir = GameManager.Instance.player.inputVec;
-        float dirX = playerDir.x < 0 ? -1 : 1; // 왼쪽 오른쪽
-        float dirY = playerDir.y < 0 ? -1 : 1; // 아래 위
+        float dirX = playerDir.x < 0 ? -1 : 1; // 왼쪽 : 오른쪽
+        float dirY = playerDir.y < 0 ? -1 : 1; // 아래 : 위
 
-        switch (transform.tag)
+        switch (transform.tag) // 오브젝트에 따른 기능
         {
             case "Ground":
                 if (diffX > diffY)
                 {
-                    transform.Translate(Vector3.right * dirX * 40);
+                    transform.Translate(Vector3.right * dirX * (mapLength * 2));
                 }
                 else if (diffX < diffY)
                 {
-                    transform.Translate(Vector3.up * dirY * 40);
+                    transform.Translate(Vector3.up * dirY * (mapLength * 2));
                 }
                 break;
 
             case "Enemy":
+                if (coll.enabled)
+                {   // 플레이어 이동방향 맞은 편에서 재등장
+                    transform.Translate(playerDir * mapLength
+                        + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0f));
+                }
                 break;
         }
     }
